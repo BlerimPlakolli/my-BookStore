3e26905687f922a1432ecd4226b12df18bea56e9
BlerimPlakolli <36608374+BlerimPlakolli@users.noreply.github.com>
2019-02-08 23:30:24 +0200

    Constructors and getter, setters were generated

--- bookstore/src/main/java/com/plakolli/bookstore/domain/Book.java
+++ bookstore/src/main/java/com/plakolli/bookstore/domain/Book.java
@@ -14,4 +14,69 @@ public class Book {
 	private String title, author, isbn;
 	private Long year;
 	private Float price;
+	
+	
+	
+	public Book() {} // constructor without parameters.
+
+	public Book(String title, String author, String isbn, Long year, Float price) {
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.year = year;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Long getYear() {
+		return year;
+	}
+
+	public void setYear(Long year) {
+		this.year = year;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+	
+	// getters and setters
+	
+	
+	
 }
