004924dabad5fa67821ed37ee52e94c1ce417164
BlerimPlakolli <36608374+BlerimPlakolli@users.noreply.github.com>
2019-02-08 23:26:42 +0200

    Entity of the book were created, id(primary key) will be generated automaticaly

--- bookstore/src/main/java/com/plakolli/bookstore/domain/Book.java
+++ bookstore/src/main/java/com/plakolli/bookstore/domain/Book.java
@@ -1,7 +1,15 @@
 package com.plakolli.bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private String title, author, isbn;
 	private Long year;
