bf00207dc56dccf2beca12ff10cec2218d1cc32f
BlerimPlakolli <36608374+BlerimPlakolli@users.noreply.github.com>
2019-02-14 20:57:53 +0200

    edit function were added to the booklist.html also the in the bookcontroller the method to heandle the get request to to edit the item were added and send the request to the save edpoint to save the changes in DB

--- bookstore/src/main/java/com/plakolli/bookstore/web/BookController.java
+++ bookstore/src/main/java/com/plakolli/bookstore/web/BookController.java
@@ -44,5 +44,11 @@ public class BookController {
 		bookRepository.deleteById(bookId);
 		return "redirect:../booklist";
 	}
+	
+	@GetMapping("/edit/{id}")
+	public String editBook(@PathVariable("id") Long bookId, Model model) {
+		model.addAttribute("book", bookRepository.findById(bookId));
+		return "editbook";
+	}
 
 }
