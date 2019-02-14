c28f9a71fda4ba587afab451a004308c06e80708
BlerimPlakolli <36608374+BlerimPlakolli@users.noreply.github.com>
2019-02-14 22:13:17 +0200

    in Category entity constuctors and getters and setters were generated

--- bookstore/src/main/java/com/plakolli/bookstore/domain/Category.java
+++ bookstore/src/main/java/com/plakolli/bookstore/domain/Category.java
@@ -4,5 +4,38 @@ public class Category {
 	
 	private Long id;
 	private String name;
+	
+	
+	public Category() {
+		super();
+	}
 
+
+	public Category(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
 }
