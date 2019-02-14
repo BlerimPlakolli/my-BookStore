46c84102550321e76c43304558a637a80a8b2fa0
BlerimPlakolli <36608374+BlerimPlakolli@users.noreply.github.com>
2019-02-14 20:12:44 +0200

    unused import were removed from BookStoreApplication

--- bookstore/src/main/java/com/plakolli/bookstore/web/BookController.java
+++ bookstore/src/main/java/com/plakolli/bookstore/web/BookController.java
@@ -1,6 +1,5 @@
 package com.plakolli.bookstore.web;
 
-import org.apache.catalina.startup.ClassLoaderFactory.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
