c9dfedd873af7887ac835721542c6c0f5bdf1834
BlerimPlakolli <36608374+BlerimPlakolli@users.noreply.github.com>
2019-03-01 22:30:29 +0200

    getters, setters and construcore for user entity

--- bookstore/src/main/java/com/plakolli/bookstore/domain/User.java
+++ bookstore/src/main/java/com/plakolli/bookstore/domain/User.java
@@ -27,6 +27,64 @@ public class User {
 	@Column(name="role", nullable=false)
 	public String role;
 	
+	//constructors
+	public User() {
+		
+	}
+	
+	public User(Long id, String username, String passwordHash, String email, String role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.passwordHash = passwordHash;
+		this.email = email;
+		this.role = role;
+	}
+	
+
+	// getters and setters
+	public Long getId() {
+		return id;
+	}
+
+	
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
 	
 	
 	
