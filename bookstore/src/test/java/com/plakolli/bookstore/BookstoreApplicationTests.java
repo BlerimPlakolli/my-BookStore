0f89ed40ebdaa3853daa83e90ddac35e43176758
BlerimPlakolli <36608374+BlerimPlakolli@users.noreply.github.com>
2019-03-08 12:57:41 +0200

    Smoke testing for book Application controller were added

--- bookstore/src/test/java/com/plakolli/bookstore/BookstoreApplicationTests.java
+++ bookstore/src/test/java/com/plakolli/bookstore/BookstoreApplicationTests.java
@@ -1,16 +1,26 @@
 package com.plakolli.bookstore;
 
+import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.plakolli.bookstore.web.BookController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookstoreApplicationTests {
+	
+	@Autowired
+	private BookController controller;
 
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
